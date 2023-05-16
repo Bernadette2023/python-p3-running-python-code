563e6cff14488cae3431444c67706975d71819d7
Bernadette Wanjiku <bernadetteb841@gmail.com>
2023-05-16 17:06:47 +0300

    Create python file

---
+++ lib/app.py
@@ -0,0 +1,14 @@
+#lib/app.py
+print ("Hello World")
+
+
+#lib/app.python
+print ("Hello World!")
+print ("Hello sun!")
+print ("Hello sky!")
+
+
+#lib/app.py
+print("Hello world!", end="")
+print("Hello sun! ", end="!!")
+print("Hello sky!", end="!!!\n")
